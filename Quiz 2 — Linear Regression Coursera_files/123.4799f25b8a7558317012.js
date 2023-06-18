a77eea4ce34d3ac74a78a0c9445b188bb907eb47
Ryan JT <ryantalbot7@gmail.com>
2023-06-18 10:50:31 -0500

    remove extraneous material

--- Quiz 2 — Linear Regression Coursera_files/123.4799f25b8a7558317012.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[123],{JISx:function(module,exports){Date.now()}}]);
-//# sourceMappingURL=123.4799f25b8a7558317012.js.map
--- Quiz 2 — Linear Regression Coursera_files/allStyles.0916c0ed881aad753de3.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([["allStyles"],[]]);
-//# sourceMappingURL=allStyles.0916c0ed881aad753de3.js.map
