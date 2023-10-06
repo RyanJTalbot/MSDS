74a74ff6a50a37dfd74715ababf89c7dc589829d
Ryan JT <ryantalbot7@gmail.com>
2023-10-05 19:23:15 -0500

    Update datga mining project

---
+++ Core/Data Mining Foundations/Data Mining Project/Project/competition_gym2023/scraping/wc15w/wc15w_bb.py
@@ -0,0 +1,36 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+
+# Define the URL of the Wikipedia page
+url = "https://en.wikipedia.org/wiki/2015_World_Artistic_Gymnastics_Championships"
+
+# Send an HTTP GET request to the URL
+response = requests.get(url)
+
+# Parse the HTML content of the page using Beautiful Soup
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# Find the table you want (in this case, we'll scrape the table at index 21)
+tables = soup.find_all('table')
+
+# Check if there are at least 22 tables on the page (since we want table 21)
+if len(tables) >= 32:
+    table_21 = tables[31]
+
+    # Extract the data from the table and store it in a list of lists
+    data = []
+    for row in table_21.find_all('tr'):
+        cells = row.find_all(['th', 'td'])
+        row_data = [cell.get_text(strip=True) for cell in cells]
+        data.append(row_data)
+
+    # Convert the data into a DataFrame
+    df = pd.DataFrame(data)
+
+    # Save the DataFrame as a CSV file
+    df.to_csv('wc15w_bb.csv', index=False, header=False)
+
+    print("Table 21 data has been scraped and saved as wc15w_bb.csv.")
+else:
+    print("Table 21 not found on the page.")
