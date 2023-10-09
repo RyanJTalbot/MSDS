8c73e72b534edf3f0b46390dd8d07f288e490c20
Ryan JT <ryantalbot7@gmail.com>
2023-10-09 10:22:10 -0500

    Update vital skills and data mining

---
+++ Core/Data Mining Foundations/Data Mining Project/Project/competition_gym2023/scraping/worldcup15cottbus/worldcup15cott_bb.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+# Specify the URL of the page containing the table
+url = 'https://thegymter.net/2015/03/20/2015-cottbus-world-challenge-cup-results/'
+
+# Use pandas to read the HTML tables on the page
+tables = pd.read_html(url)
+
+# Assuming the table you want is the first one on the page, you can access it like this:
+df = tables[2]
+
+# Drop rows with all NaN values
+df.dropna(how='all', inplace=True)
+
+# Reset the index
+df.reset_index(drop=True, inplace=True)
+
+# Display the DataFrame
+print(df)
+# Save the DataFrame to a CSV file
+df.to_csv('/Users/ryantalbot/Desktop/MSDS/worldcup2015cott_bb.csv', index=False)
