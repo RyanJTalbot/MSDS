48fe189cc619efee129a4114fc70737d7c44edd4
Ryan JT <ryantalbot7@gmail.com>
2023-10-12 12:06:27 -0500

    Update data mining project

---
+++ Core/Data Mining Foundations/Data Mining Project/Project/competition_gym2023/scraping/pacific_rim_championship16/prc16bb.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+# Specify the URL of the page containing the table
+url = 'https://thegymter.net/2016/04/10/2016-pacific-rim-championships-results/'
+
+# Use pandas to read the HTML tables on the page
+tables = pd.read_html(url)
+
+# Assuming the table you want is the first one on the page, you can access it like this:
+df = tables[3]
+
+# Drop rows with all NaN values
+df.dropna(how='all', inplace=True)
+
+# Reset the index
+df.reset_index(drop=True, inplace=True)
+
+# Display the DataFrame
+print(df)
+# Save the DataFrame to a CSV file
+df.to_csv(
+    '/Users/ryantalbot/Desktop/MSDS/pac_rim_championshiop2016bb.csv', index=False)
