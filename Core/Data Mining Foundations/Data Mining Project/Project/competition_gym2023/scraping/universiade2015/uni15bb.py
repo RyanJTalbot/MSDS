6b901ebfe24c1a3cdff8449181e33d3e09d8d4f5
Ryan JT <ryantalbot7@gmail.com>
2023-10-15 20:37:42 -0500

    Update data mining proj

---
+++ Core/Data Mining Foundations/Data Mining Project/Project/competition_gym2023/scraping/universiade2015/uni15bb.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+# Specify the URL of the page containing the table
+url = 'https://thegymter.net/2015/07/05/2015-summer-universiade-results/'
+
+# Use pandas to read the HTML tables on the page
+tables = pd.read_html(url)
+
+# Assuming the table you want is the first one on the page, you can access it like this:
+df = tables[3]
+
+# Drop rows with all NaN values
+df.dropna(how='all', inplace=True)
+
+# Reset the index
+df.reset_index(drop=True, inplace=True)
+
+# Display the DataFrame
+print(df)
+# Save the DataFrame to a CSV file
+df.to_csv('/Users/ryantalbot/Desktop/MSDS/universiade2015bb.csv', index=False)
