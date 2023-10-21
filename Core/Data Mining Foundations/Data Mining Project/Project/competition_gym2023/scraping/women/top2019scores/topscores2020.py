dcaf5ebae0b0d08c3da4c024e95e46287f09b3eb
Ryan JT <ryantalbot7@gmail.com>
2023-10-21 10:49:22 -0500

    Update data mining proj

---
+++ Core/Data Mining Foundations/Data Mining Project/Project/competition_gym2023/scraping/women/top2019scores/topscores2020.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+# Specify the URL of the page containing the tables
+url = 'https://thegymter.net/the-best-scores-in-2019/'
+
+# Use pandas to read the HTML tables on the page
+tables = pd.read_html(url)
+
+# Loop through each table and save it as a separate CSV file
+for i, table in enumerate(tables):
+    # Drop rows with all NaN values
+    table.dropna(how='all', inplace=True)
+
+    # Reset the index
+    table.reset_index(drop=True, inplace=True)
+
+    # Save the DataFrame to a CSV file
+    filename = f'/Users/ryantalbot/Desktop/MSDS/table_{i}.csv'
+    table.to_csv(filename, index=False)
+    print(f'Saved table {i} to {filename}')
