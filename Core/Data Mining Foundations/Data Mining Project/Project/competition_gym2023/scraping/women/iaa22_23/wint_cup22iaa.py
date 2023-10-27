d87e0e7eb1381f8aa63754ce29e16446ff956770
Ryan JT <ryantalbot7@gmail.com>
2023-10-27 10:08:48 -0500

    Update vital skills and data minin proj

---
+++ Core/Data Mining Foundations/Data Mining Project/Project/competition_gym2023/scraping/women/iaa22_23/wint_cup22iaa.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+# Specify the URL of the page containing the tables
+url = 'https://thegymter.net/2022/03/01/2022-winter-cup-results/'
+
+# Send a GET request to the URL and parse the content with BeautifulSoup
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# Find all tables on the page
+tables = soup.find_all('table')
+
+# Loop through each table and save them to separate CSV files
+for i, table in enumerate(tables):
+    df = pd.read_html(str(table))[0]
+
+    # Drop rows with all NaN values
+    df.dropna(how='all', inplace=True)
+
+    # Reset the index
+    df.reset_index(drop=True, inplace=True)
+
+    # Save the DataFrame to a CSV file
+    csv_filename = f'table_{i}.csv'
+    df.to_csv(csv_filename, index=False)
+
+    print(f'Table {i} saved as {csv_filename}')
